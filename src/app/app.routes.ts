1330cfd32716b0136272d164b3d72bb9382a943b
Massimiliano <masdelmon@hotmail.it>
2019-02-17 20:07:41 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SubrouteComponent } from './subroute/subroute.component';
+import { VideoComponent } from './app/video.component';
+import { ExperiencesComponent } from './app/experiences.component';
 
 
 export const ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'subroute', component: SubrouteComponent },
-  { path: 'experience', component: SubrouteComponent },
-  { path: 'video', component: SubrouteComponent }
+  { path: 'experiences', component: ExperiencesComponent },
+  { path: 'video', component: VideoComponent }
 ];
