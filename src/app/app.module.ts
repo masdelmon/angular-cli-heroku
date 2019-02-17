d804614ae0a4c18ba3ab65840105c31246421b93
Massimiliano <masdelmon@hotmail.it>
2019-02-17 19:38:50 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
+import { EmbedVideo } from 'ngx-embed-video';
+
 import { AppComponent } from './app.component';
 import { SubrouteComponent } from './subroute/subroute.component';
 
@@ -17,6 +20,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    HttpClientModule,
+    EmbedVideo.forRoot()
     BrowserModule,
     FormsModule,
     HttpModule,
