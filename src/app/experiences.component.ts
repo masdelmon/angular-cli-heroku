c7a9218c84c60e102e9811d41aa9e35b38a1768a
Massimiliano <masdelmon@hotmail.it>
2019-02-17 20:09:59 +0100

    Update experiences.component.ts

--- src/app/experiences.component.ts
+++ src/app/experiences.component.ts
@@ -1,1 +1,9 @@
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-experiences',
+  templateUrl: './experiences.component.html',
+  styleUrls: ['./experiences.component.css']
+})
+export class ExperiencesComponent {
+}
