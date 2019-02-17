04096480024781b2f3ae3e9cf73e32be239bbdfc
Massimiliano <masdelmon@hotmail.it>
2019-02-17 19:13:26 +0100

    Update subroute.component.ts

--- src/app/subroute/subroute.component.ts
+++ src/app/subroute/subroute.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subroute.component.css']
 })
 export class SubrouteComponent implements OnInit {
-
+  section = "Personal Info";
   constructor() { }
 
   ngOnInit() {
