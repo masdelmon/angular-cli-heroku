8f8aa5a9905860b7714acf15ff0722233cd5fbf7
Massimiliano <masdelmon@hotmail.it>
2019-02-18 19:21:46 +0100

    Update video.component.ts

--- src/app/video.component.ts
+++ src/app/video.component.ts
@@ -1,18 +1,11 @@
-import { Component } from '@angular/core';/*
-import { EmbedVideoService } from 'ngx-embed-video';*/
+import { Component } from '@angular/core';
  
 @Component({
   selector: 'video-component',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.css']
 })
-export class VideoComponent {/*
-  yt_iframe_html:any;
-  youtubeUrl = 'https://www.youtube.com/watch?v=TXaY1VY2FP8';*/
-  
-  /* iHhcHTlGtRs';https://m.youtube.com/watch?v=TXaY1VY2FP8 */
- 
-  constructor(/*private embedService: EmbedVideoService*/) {/*
-    this.yt_iframe_html = this.embedService.embed(this.youtubeUrl);*/
+export class VideoComponent {
+  constructor() {
   }
 }
