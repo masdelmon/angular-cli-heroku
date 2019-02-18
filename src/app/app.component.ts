bbaabe6e877e7448fd099d2e38f6a4ed8c915189
Massimiliano <masdelmon@hotmail.it>
2019-02-18 13:41:23 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,16 @@ import 'rxjs/add/operator/map';
 
 export class AppComponent {
    constructor(private http: Http) { }
+   experiences;
+  
    ngOnInit() {
       this.http.get("http://jsonplaceholder.typicode.com/masdelmon/api-cv").
-      map((response) ⇒ response.json()).
-      subscribe((data) ⇒ console.log(data))
+      map(
+         (response) ⇒ response.json()
+      ).
+      subscribe(
+         (data) ⇒ {this.displaydata(data);}
+      )
    }
+   displaydata(data) {this.experiences = data;}
+}
