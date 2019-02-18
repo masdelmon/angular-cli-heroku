0bc4e50369ff91f9b36f45657ed4c104a4786a14
Massimiliano <masdelmon@hotmail.it>
2019-02-18 14:02:32 +0100

    Update experiences.component.ts

--- src/app/experiences.component.ts
+++ src/app/experiences.component.ts
@@ -15,7 +15,7 @@ export class ExperiencesComponent {
   constructor(private http: Http) { }
   
    ngOnInit() {
-      this.http.get("http://jsonplaceholder.typicode.com/masdelmon/api-cv").
+      this.http.get("http://jsonplaceholder.typicode.com/masdelmon/api-cv/experiences").
       map(
          (response) ⇒ response.json()
       ).
