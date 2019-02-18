1f854b4c8f6882682a1c676e161582d2ba80ba92
Massimiliano <masdelmon@hotmail.it>
2019-02-18 13:47:30 +0100

    Update experiences.component.ts

--- src/app/experiences.component.ts
+++ src/app/experiences.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-experiences',
   templateUrl: './experiences.component.html',
   styleUrls: ['./experiences.component.css']
 })
-export class ExperiencesComponent {
+
+export class AppComponent {
+   constructor(private http: Http) { }
+   experiences;
+  
+   ngOnInit() {
+      this.http.get("http://jsonplaceholder.typicode.com/masdelmon/api-cv").
+      map(
+         (response) ⇒ response.json()
+      ).
+      subscribe(
+         (data) ⇒ {this.displaydata(data);}
+      )
+   }
+   displaydata(data) {this.experiences = data;}
 }
