00b7057cb445f7de0d37387b5fbf7e5642e96d34
Massimiliano <masdelmon@hotmail.it>
2019-02-18 19:18:19 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SubrouteComponent } from './subroute/subroute.component';
-
 import { ROUTES } from './app.routes';
 
 import { HomeComponent } from './home/home.component';
@@ -24,7 +22,6 @@ import { ExperiencesComponent } from './experiences.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
