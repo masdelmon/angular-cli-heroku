709f70a775937d7425dbc20347d16f40846923fe
Massimiliano <masdelmon@hotmail.it>
2019-02-18 19:52:08 +0100

    Update experiences.component.ts

--- src/app/experiences.component.ts
+++ src/app/experiences.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-experiences',
@@ -8,7 +8,7 @@ import { Http } from '@angular/http';
 })
 
 export class ExperiencesComponent {
-   constructor(private http: Http) { }
+   constructor(private http: HttpClient) { }
    exp;
    ngOnInit() 
    {
