79fb713fd51ac213e82aaf7ba0b36d91b27276de
Massimiliano <masdelmon@hotmail.it>
2019-03-19 11:34:29 +0100

    Update experiences.component.ts

--- src/app/experiences.component.ts
+++ src/app/experiences.component.ts
@@ -11,13 +11,15 @@ export class ExperiencesComponent {
    constructor(private http: HttpClient) { }
    experiences;
    section = "Work experiences";
+   exp_error:Boolean = false;
   
    ngOnInit() 
    {
      /*
       this.http.get("http://jsonplaceholder.typicode.com/users").map((response) ⇒ response.json()).subscribe((data) ⇒ {this.displaydata(data);})
       */
-      this.http.get('https://my-json-server.typicode.com/masdelmon/api-cv/experiences').subscribe(data => {this.displaydata(data);})
+      this.http.get('https://my-json-server.typicode.com/masdelmon/api-cv/experiences').subscribe(
+        data => {this.displaydata(data);},err => { this.exp_error = true })
  
    }
    displaydata(data) {this.experiences = data;}
